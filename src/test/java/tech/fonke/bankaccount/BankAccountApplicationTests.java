0ab7a9b1a80e48a6fad00ca050dec1a29ae8129d
NJOMBOSS <fonke1982@gmail.com>
2024-03-18 10:55:40 +0100

    first commit

---
+++ src/test/java/tech/fonke/bankaccount/BankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.fonke.bankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
